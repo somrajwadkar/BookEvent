a18a6da91d18f81e6aa61ecb7691f8c54c21ae81
somraj wadkar <somrajwadkar2@gmail.com>
2026-07-22 10:31:03 +0530

    the new commit and File Changes

--- server/bookings.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { bookEvent, confirmBooking, getMyBookings, cancelBooking, sendBookingOTP } = require('../controllers/bookingController');
-const { protect, admin } = require('../middleware/auth');
