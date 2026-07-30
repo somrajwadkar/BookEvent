1dc5e88ae80607ae11f4593c9edde8aafefcaa52
somraj wadkar <somrajwadkar2@gmail.com>
2026-07-31 00:15:38 +0530

    the new Admin

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -19,3 +19,13 @@ const protect = async (req, res, next) => {
         res.status(401).json({ message: 'Not authorized, no token' });
     }
 };
+
+const admin = (req, res, next) => {
+    if (req.user && req.user.role === 'admin') {
+        next();
+    } else {
+        res.status(403).json({ message: 'Not authorized as an admin' });
+    }
+};
+
+module.exports = { protect, admin };
