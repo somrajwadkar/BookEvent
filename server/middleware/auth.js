066a5bac6a02edbfde516af3645c7e9360d64a92
somraj wadkar <somrajwadkar2@gmail.com>
2026-08-01 00:51:52 +0530

    the latest

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -9,11 +9,11 @@ const protect = async (req, res, next) => {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = await User.findById(decoded.id).select('-password');
             if (!req.user) {
-                return res.status(401).json({ message: 'Not authorized, user not found' });
+                return res.status(401).json({ message: 'Not authorized' });
             }
             next();
         } catch (error) {
-            res.status(401).json({ message: 'Not authorized, token failed' });
+            res.status(401).json({ message: 'Not authorized, token foun' });
         }
     } else {
         res.status(401).json({ message: 'Not authorized, no token' });
