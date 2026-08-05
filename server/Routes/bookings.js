3b1f474d67c63da7aa64fe90d31b55b2baf2cc10
somraj wadkar <somrajwadkar2@gmail.com>
2026-08-05 11:39:25 +0530

    the latest

--- server/Routes/bookings.js
+++ server/Routes/bookings.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const { bookEvent, confirmBooking, getMyBookings, cancelBooking, sendBookingOTP } = require('../controllers/bookingController');
 const { protect, admin } = require('../middleware/auth');
 
-router.post('/send-otp', protect, sendBookingOTP);
+
+//All bookings Related Routers
+router.post('/send-otp', protect, sendBookingOTP); 
 router.post('/', protect, bookEvent);
 router.put('/:id/confirm', protect, admin, confirmBooking);
 router.get('/my', protect, getMyBookings);
