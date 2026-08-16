1cc5db610076177fc72e2b51cb79f3c5d7e9bdc9
somraj wadkar <somrajwadkar2@gmail.com>
2026-08-16 23:45:56 +0530

    the new Commit

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { register, login, verifyOTP } = require('../controllers/authController');
 
+//Authorization and Authentication and Security Related features  
 router.post('/register', register);
 router.post('/login', login);
 router.post('/verify-otp', verifyOTP);
